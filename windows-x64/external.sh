8864ac76e04b1cc5003fb834e88243ed7bfe7fcf
Jason Millard <jsm174@gmail.com>
2025-01-28 22:16:13 -0500

    misc: add support for libpinmame

--- windows-x64/external.sh
+++ windows-x64/external.sh
@@ -2,15 +2,31 @@
 
 set -e
 
+PINMAME_SHA=872f7796d4c96567e38e764d4f5f5224efc7c9d4
 SDL_SHA=535d80badefc83c5c527ec5748f2a20d6a9310fe
 SDL_IMAGE_SHA=4ff27afa450eabd2a827e49ed86fab9e3bf826c5
 SDL_TTF_SHA=5e651ee5054a95fdb91702ba1b398d751155febc
 SDL_MIXER_SHA=af6a29df4e14c6ce72608b3ccd49cf35e1014255
 
-CACHE_NAME="SDL-${SDL_SHA}-${SDL_IMAGE_SHA}-${SDL_TTF_SHA}-${SDL_MIXER_SHA}"
-
 mkdir -p tmp/build-libs/windows-x64
 mkdir -p tmp/runtime-libs/windows-x64
+mkdir -p tmp/include
+
+curl -sL https://github.com/vbousquet/pinmame/archive/${PINMAME_SHA}.zip -o pinmame-${PINMAME_SHA}.zip
+unzip pinmame-${PINMAME_SHA}.zip
+cd pinmame-${PINMAME_SHA}
+cp cmake/libpinmame/CMakelists.txt .
+cmake -G "Visual Studio 17 2022" \
+   -DPLATFORM=win \
+   -DARCH=x64 \
+   -DBUILD_SHARED=ON \
+   -DBUILD_STATIC=OFF \
+   -B build
+cmake --build build --config Release
+cp build/Release/pinmame64.lib ../tmp/build-libs/windows-x64
+cp build/Release/pinmame64.dll ../tmp/runtime-libs/windows-x64
+cp src/libpinmame/libpinmame.h ../tmp/include
+cd ..
 
 curl -sL https://github.com/libsdl-org/SDL/archive/${SDL_SHA}.zip -o SDL-${SDL_SHA}.zip
 unzip SDL-${SDL_SHA}.zip
@@ -24,6 +40,8 @@ cmake -G "Visual Studio 17 2022" \
 cmake --build build --config Release
 cp build/Release/SDL364.lib ../tmp/build-libs/windows-x64
 cp build/Release/SDL364.dll ../tmp/runtime-libs/windows-x64
+mkdir -p ../tmp/include/SDL3
+cp -r include/SDL3/* ../tmp/include/SDL3
 cd ..
 
 curl -sL https://github.com/libsdl-org/SDL_image/archive/${SDL_IMAGE_SHA}.zip -o SDL_image-${SDL_IMAGE_SHA}.zip
@@ -43,6 +61,8 @@ cmake -G "Visual Studio 17 2022" \
 cmake --build build --config Release
 cp build/Release/SDL3_image64.lib ../tmp/build-libs/windows-x64
 cp build/Release/SDL3_image64.dll ../tmp/runtime-libs/windows-x64
+mkdir -p ../tmp/include/SDL3_image
+cp -r include/SDL3_image/* ../tmp/include/SDL3_image
 cd ..
 
 curl -sL https://github.com/libsdl-org/SDL_ttf/archive/${SDL_TTF_SHA}.zip -o SDL_ttf-${SDL_TTF_SHA}.zip
@@ -60,6 +80,8 @@ cmake -G "Visual Studio 17 2022" \
 cmake --build build --config Release
 cp build/Release/SDL3_ttf64.lib ../tmp/build-libs/windows-x64
 cp build/Release/SDL3_ttf64.dll ../tmp/runtime-libs/windows-x64
+mkdir -p ../tmp/include/SDL3_ttf
+cp -r include/SDL3_ttf/* ../tmp/include/SDL3_ttf
 cd ..
 
 curl -sL https://github.com/libsdl-org/SDL_mixer/archive/${SDL_MIXER_SHA}.zip -o SDL_mixer-${SDL_MIXER_SHA}.zip
@@ -76,4 +98,6 @@ cmake -G "Visual Studio 17 2022" \
 cmake --build build --config Release
 cp build/Release/SDL3_mixer64.lib ../tmp/build-libs/windows-x64
 cp build/Release/SDL3_mixer64.dll ../tmp/runtime-libs/windows-x64
+mkdir -p ../tmp/include/SDL3_mixer
+cp -r include/SDL3_mixer/* ../tmp/include/SDL3_mixer
 cd ..
--- windows-x86/external.sh
+++ windows-x86/external.sh
@@ -2,15 +2,32 @@
 
 set -e
 
+PINMAME_SHA=872f7796d4c96567e38e764d4f5f5224efc7c9d4
 SDL_SHA=535d80badefc83c5c527ec5748f2a20d6a9310fe
 SDL_IMAGE_SHA=4ff27afa450eabd2a827e49ed86fab9e3bf826c5
 SDL_TTF_SHA=5e651ee5054a95fdb91702ba1b398d751155febc
 SDL_MIXER_SHA=af6a29df4e14c6ce72608b3ccd49cf35e1014255
 
-CACHE_NAME="SDL-${SDL_SHA}-${SDL_IMAGE_SHA}-${SDL_TTF_SHA}-${SDL_MIXER_SHA}"
-
 mkdir -p tmp/build-libs/windows-x86
 mkdir -p tmp/runtime-libs/windows-x86
+mkdir -p tmp/include
+
+curl -sL https://github.com/vbousquet/pinmame/archive/${PINMAME_SHA}.zip -o pinmame-${PINMAME_SHA}.zip
+unzip pinmame-${PINMAME_SHA}.zip
+cd pinmame-${PINMAME_SHA}
+cp cmake/libpinmame/CMakelists.txt .
+cmake -G "Visual Studio 17 2022" \
+   -A Win32 \
+   -DPLATFORM=win \
+   -DARCH=x86 \
+   -DBUILD_SHARED=ON \
+   -DBUILD_STATIC=OFF \
+   -B build
+cmake --build build --config Release
+cp build/Release/pinmame.lib ../tmp/build-libs/windows-x86
+cp build/Release/pinmame.dll ../tmp/runtime-libs/windows-x86
+cp src/libpinmame/libpinmame.h ../tmp/include
+cd ..
 
 curl -sL https://github.com/libsdl-org/SDL/archive/${SDL_SHA}.zip -o SDL-${SDL_SHA}.zip
 unzip SDL-${SDL_SHA}.zip
@@ -23,6 +40,8 @@ cmake -G "Visual Studio 17 2022" -A Win32 \
 cmake --build build --config Release
 cp build/Release/SDL3.lib ../tmp/build-libs/windows-x86
 cp build/Release/SDL3.dll ../tmp/runtime-libs/windows-x86
+mkdir -p ../tmp/include/SDL3
+cp -r include/SDL3/* ../tmp/include/SDL3
 cd ..
 
 curl -sL https://github.com/libsdl-org/SDL_image/archive/${SDL_IMAGE_SHA}.zip -o SDL_image-${SDL_IMAGE_SHA}.zip
@@ -41,6 +60,8 @@ cmake -G "Visual Studio 17 2022" -A Win32 \
 cmake --build build --config Release
 cp build/Release/SDL3_image.lib ../tmp/build-libs/windows-x86
 cp build/Release/SDL3_image.dll ../tmp/runtime-libs/windows-x86
+mkdir -p ../tmp/include/SDL3_image
+cp -r include/SDL3_image/* ../tmp/include/SDL3_image
 cd ..
 
 curl -sL https://github.com/libsdl-org/SDL_ttf/archive/${SDL_TTF_SHA}.zip -o SDL_ttf-${SDL_TTF_SHA}.zip
@@ -57,6 +78,8 @@ cmake -G "Visual Studio 17 2022" -A Win32 \
 cmake --build build --config Release
 cp build/Release/SDL3_ttf.lib ../tmp/build-libs/windows-x86
 cp build/Release/SDL3_ttf.dll ../tmp/runtime-libs/windows-x86
+mkdir -p ../tmp/include/SDL3_ttf
+cp -r include/SDL3_ttf/* ../tmp/include/SDL3_ttf
 cd ..
 
 curl -sL https://github.com/libsdl-org/SDL_mixer/archive/${SDL_MIXER_SHA}.zip -o SDL_mixer-${SDL_MIXER_SHA}.zip
@@ -72,4 +95,6 @@ cmake -G "Visual Studio 17 2022" -A Win32 \
 cmake --build build --config Release
 cp build/Release/SDL3_mixer.lib ../tmp/build-libs/windows-x86
 cp build/Release/SDL3_mixer.dll ../tmp/runtime-libs/windows-x86
+mkdir -p ../tmp/include/SDL3_mixer
+cp -r include/SDL3_mixer/* ../tmp/include/SDL3_mixer
 cd ..
